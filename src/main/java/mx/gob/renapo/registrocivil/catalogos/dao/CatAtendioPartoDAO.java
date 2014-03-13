207eb300c80845bcd6f073a3d56db2f0ec35cb29
Jose Salvador Huerta Acosta <shuerta@sintelti.mx>
2014-03-12 19:35:26 -0600

    se sube paquete dao para catalogos

---
+++ src/main/java/mx/gob/renapo/registrocivil/catalogos/dao/CatAtendioPartoDAO.java
@@ -0,0 +1,10 @@
+package mx.gob.renapo.registrocivil.catalogos.dao;
+
+import mx.gob.renapo.registrocivil.catalogos.entity.CatAtendioParto;
+import mx.gob.renapo.registrocivil.comun.dao.GeneralDAO;
+
+public interface CatAtendioPartoDAO extends GeneralDAO <CatAtendioParto>{
+	
+	
+
+}
---
+++ src/main/java/mx/gob/renapo/registrocivil/catalogos/dao/impl/CatAtendioPartoDAOImpl.java
@@ -0,0 +1,11 @@
+package mx.gob.renapo.registrocivil.catalogos.dao.impl;
+
+import mx.gob.renapo.registrocivil.catalogos.dao.CatAtendioPartoDAO;
+import mx.gob.renapo.registrocivil.catalogos.entity.CatAtendioParto;
+import mx.gob.renapo.registrocivil.comun.dao.impl.GeneralDAOImpl;
+
+public class CatAtendioPartoDAOImpl  extends GeneralDAOImpl<CatAtendioParto> implements CatAtendioPartoDAO{
+	
+	
+
+}
