c397b398b5cc175f7caddf3e41a7cc29612c0c6b
Alejandro Diaz <adsolis@sintelti.com.mx>
2014-03-18 12:32:59 -0600

    se quita acento de comentarios

--- src/main/java/mx/gob/renapo/registrocivil/catalogos/dto/CatalogoDTO.java
+++ src/main/java/mx/gob/renapo/registrocivil/catalogos/dto/CatalogoDTO.java
@@ -7,7 +7,7 @@ import lombok.Data;
  * User: Alejandro Diaz Solis
  * Date: 3/13/14
  * Time: 12:02 PM
- * DTO para catalogos, este DTO servirá para todos aquellos catalogos con las
+ * DTO para catalogos, este DTO servira para todos aquellos catalogos con las
  * mismas caracteristicas  (AtendioParto, Comparece, DestinoCadave, Escolaridad,
  * EstadoCivil, LugarParto, Parentesco, Puesto, Regimen, SituacionLaboral, TipoDivorcio,
  * TipoDocumento, TipoOperacion, TipoParto, TipoSentencia, TipoNotaMarginal, TipoOficialia
