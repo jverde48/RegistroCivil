6712c7fb935e0ba2b8c3d37494ec3ee2ec334d03
Alejandro Diaz <adsolis@sintelti.com.mx>
2014-03-18 20:35:27 -0600

    se agrega propiedad de municipios a DTO de estado

--- src/main/java/mx/gob/renapo/registrocivil/catalogos/dto/EstadoDTO.java
+++ src/main/java/mx/gob/renapo/registrocivil/catalogos/dto/EstadoDTO.java
@@ -1,13 +1,14 @@
 package mx.gob.renapo.registrocivil.catalogos.dto;
 
 import lombok.Data;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
  * User: Aleandro Diaz Solis
  * Date: 3/13/14
  * Time: 2:02 PM
- * DTO dde Estado (Entidad Federativa), este DTO se puede utilizar t
+ * DTO de Estado (Entidad Federativa), este DTO se puede utilizar t
  * anto para el catalogo de INEGI como el que maneja RENAPO
  */
 @Data
@@ -17,6 +18,7 @@ public class EstadoDTO {
     private Long idRenapo;
     private String nombreEstado;
     private PaisDTO pais;
+    private List<MunicipioDTO> municipios;
 
 
 }
