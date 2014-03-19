146a53fd970a0a290939f1df09ab9dfbb9312b3c
Alejandro Diaz <adsolis@sintelti.com.mx>
2014-03-18 20:19:03 -0600

    se agrega de dto de tipo operacion

---
+++ src/main/java/mx/gob/renapo/registrocivil/catalogos/dto/CatTipoOperacionDTO.java
@@ -0,0 +1,11 @@
+package mx.gob.renapo.registrocivil.catalogos.dto;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alejandro Diaz Solis
+ * Date: 3/18/14
+ * Time: 7:23 PM
+ * DTO del catalogo de tipo de operacion
+ */
+public class CatTipoOperacionDTO extends CatalogoDTO{
+}
