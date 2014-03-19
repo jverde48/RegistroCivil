dc6caf445627bf1fd23f4a2d3ff04cca403909bf
Alejandro Diaz <adsolis@sintelti.com.mx>
2014-03-19 17:44:46 -0600

    se agregan entities de inegi de estado y municipio y catalogo de pais de renapo

---
+++ src/main/java/mx/gob/renapo/registrocivil/catalogos/entity/CatPais.java
@@ -0,0 +1,33 @@
+package mx.gob.renapo.registrocivil.catalogos.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alejandro Diaz Solis
+ * Date: 3/19/14
+ * Time: 12:14 PM
+ * Entity del catalogo de Pais
+ */
+@Entity
+@Table(name = "CAT_PAIS")
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class CatPais extends Catalogo implements Serializable {
+
+    public CatPais() {
+
+    }
+
+    @OneToMany
+    private Set<CatEstado> estados;
+
+
+}
