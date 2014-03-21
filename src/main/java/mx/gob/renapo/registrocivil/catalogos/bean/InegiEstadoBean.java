3ff5287cfbc7b16d00bb1f641245187a02c449cb
Jose Salvador Huerta Acosta <shuerta@sintelti.mx>
2014-03-21 11:44:38 -0600

    se suben bean de catalogos

---
+++ src/main/java/mx/gob/renapo/registrocivil/catalogos/bean/InegiEstadoBean.java
@@ -0,0 +1,35 @@
+package mx.gob.renapo.registrocivil.catalogos.bean;
+
+public class InegiEstadoBean extends CatalogoBean {
+
+	@Override
+	public void crear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actualizar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void findById() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void findAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
