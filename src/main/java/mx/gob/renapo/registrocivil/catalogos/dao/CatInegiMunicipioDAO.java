1ea3cc1b2675d5ef8629ae21a39e22ff63b559f1
jverde <jverde@sintelti.com.mx>
2014-03-23 02:58:36 -0600

    Se sube mapeo de de localidades a municipio

---
+++ src/main/java/mx/gob/renapo/registrocivil/catalogos/dao/CatInegiMunicipioDAO.java
@@ -0,0 +1,15 @@
+package mx.gob.renapo.registrocivil.catalogos.dao;
+
+import mx.gob.renapo.registrocivil.catalogos.entity.CatInegiMunicipio;
+import mx.gob.renapo.registrocivil.comun.dao.GeneralDAO;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: green-4
+ * Date: 23/03/14
+ * Time: 01:28
+ * To change this template use File | Settings | File Templates.
+ */
+public interface CatInegiMunicipioDAO extends GeneralDAO<CatInegiMunicipio> {
+
+}
