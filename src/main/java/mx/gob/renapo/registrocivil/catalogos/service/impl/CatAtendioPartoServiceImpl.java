bca18f0a2d9da683900abc8222eddb5e0ad1ca3c
Alejandro Diaz Solis <adsolis@sintelti.com.mx>
2014-04-04 19:11:48 -0600

    se agrega clase faltante al commit en el que se agrego manejo de excepciones

--- src/main/java/mx/gob/renapo/registrocivil/catalogos/service/impl/CatAtendioPartoServiceImpl.java
+++ src/main/java/mx/gob/renapo/registrocivil/catalogos/service/impl/CatAtendioPartoServiceImpl.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+
 import lombok.Data;
 import mx.gob.renapo.registrocivil.catalogos.dao.CatAtendioPartoDAO;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import mx.gob.renapo.registrocivil.catalogos.entity.CatAtendioParto;
 import mx.gob.renapo.registrocivil.catalogos.service.CatAtendioPartoService;
 import mx.gob.renapo.registrocivil.catalogos.dto.CatAtendioPartoDTO;
@@ -15,17 +19,24 @@ import mx.gob.renapo.registrocivil.catalogos.dto.CatAtendioPartoDTO;
 @Data
 @Service
 public class CatAtendioPartoServiceImpl implements CatAtendioPartoService {
+	
+	private static Logger logger = Logger.getLogger(CatLugarPartoServiceImpl.class);
 
     @Autowired
     private CatAtendioPartoDAO catAtendioPartoDAO;
 
 	@Override
 	public boolean crearAtendioParto(String descripcion) {
-		System.out.println(descripcion);
 		CatAtendioParto atendioParto = new CatAtendioParto();
-		atendioParto.setDescripcion(descripcion);
-		atendioParto.setVersion(1L);
-		catAtendioPartoDAO.guardarRegistro(atendioParto);
+		
+		try {
+			atendioParto.setDescripcion(descripcion);
+			atendioParto.setVersion(1L);
+			catAtendioPartoDAO.guardarRegistro(atendioParto);
+		}catch(Exception e) {
+			logger.error("Error: " + e);
+		}
+		
 		return false;
 	}
 
@@ -44,32 +55,43 @@ public class CatAtendioPartoServiceImpl implements CatAtendioPartoService {
 
 	@Override
 	public CatAtendioPartoDTO findById(Long id) {
-        CatAtendioParto atendioParto =catAtendioPartoDAO.recuperarRegistro(id);
-        CatAtendioPartoDTO atendioPartoDTO = new CatAtendioPartoDTO();
-        if(atendioParto!=null) {
-            atendioPartoDTO.setId(atendioParto.getId());
-            atendioPartoDTO.setDescripcion(atendioParto.getDescripcion());
-        }
+		 CatAtendioPartoDTO atendioPartoDTO = new CatAtendioPartoDTO();
+		 try {
+			 CatAtendioParto atendioParto =catAtendioPartoDAO.recuperarRegistro(id);      
+		        if(atendioParto!=null) {
+		            atendioPartoDTO.setId(atendioParto.getId());
+		            atendioPartoDTO.setDescripcion(atendioParto.getDescripcion());
+		        } 
+		 }catch(Exception e) {
+			 logger.error("Error: " +  e);
+		 }
+       
 		return atendioPartoDTO;
 	}
 
 	@Override
 	public List<CatAtendioPartoDTO> findAll() {
+		List<CatAtendioPartoDTO> atendioPartoDTOList = new ArrayList<CatAtendioPartoDTO>();
+		
+		try {
+			List<CatAtendioParto> atendioPartoList = catAtendioPartoDAO.listarRegistros();
+	        
+	        CatAtendioPartoDTO atendioPartoDTO = null;
+	        if(atendioPartoList.size()>0) {
+	            for(CatAtendioParto atendioParto: atendioPartoList) {
 
-        List<CatAtendioParto> atendioPartoList = catAtendioPartoDAO.listarRegistros();
-        List<CatAtendioPartoDTO> atendioPartoDTOList = new ArrayList<CatAtendioPartoDTO>();
-        CatAtendioPartoDTO atendioPartoDTO = null;
-        if(atendioPartoList.size()>0) {
-            for(CatAtendioParto atendioParto: atendioPartoList) {
+	                atendioPartoDTO = new CatAtendioPartoDTO();
+	                atendioPartoDTO.setId(atendioParto.getId());
+	                atendioPartoDTO.setDescripcion(atendioParto.getDescripcion());
+	                atendioPartoDTOList.add(atendioPartoDTO);
 
-                atendioPartoDTO = new CatAtendioPartoDTO();
-                atendioPartoDTO.setId(atendioParto.getId());
-                atendioPartoDTO.setDescripcion(atendioParto.getDescripcion());
-                atendioPartoDTOList.add(atendioPartoDTO);
+	            }
 
-            }
-
-        }
+	        }
+		}catch(Exception e) {
+			logger.error("Error: " + e);
+		}
+        
 
         return atendioPartoDTOList;
 	}
