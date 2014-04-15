fec1fe30f2ff71d31fa7c9415c39fff30efe800f
shuerta <shuerta@sintelti.mx>
2014-04-14 19:28:37 -0500

    cambios en bean de sentencia

---
+++ src/main/java/mx/gob/renapo/registrocivil/actos/sentencia/dao/impl/TipoSentenciaDAOImpl.java
@@ -0,0 +1,17 @@
+package mx.gob.renapo.registrocivil.actos.sentencia.dao.impl;
+
+import mx.gob.renapo.registrocivil.actos.sentencia.dao.TipoSentenciaDAO;
+import mx.gob.renapo.registrocivil.actos.sentencia.entity.Sentencia;
+import mx.gob.renapo.registrocivil.comun.dao.impl.GeneralDAOImpl;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: USER
+ * Date: 11/04/14
+ * Time: 07:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+public class TipoSentenciaDAOImpl extends GeneralDAOImpl<Sentencia> implements TipoSentenciaDAO{
+}
