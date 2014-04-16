dd2108cae572562d4eff067801675746ceeb012c
Abel Molina <amolina@sintelti.com.mx>
2014-04-16 16:37:08 -0500

    se agrega mensaje de error al no guardarse correctamente un registro historico en acto defuncion

--- src/main/java/mx/gob/renapo/registrocivil/actos/defuncion/bean/DefuncionHistoricoBean.java
+++ src/main/java/mx/gob/renapo/registrocivil/actos/defuncion/bean/DefuncionHistoricoBean.java
@@ -3,6 +3,7 @@ package mx.gob.renapo.registrocivil.actos.defuncion.bean;
 import lombok.Data;
 import mx.gob.renapo.registrocivil.util.ConstantesComunes;
 import org.apache.log4j.Logger;
+import org.primefaces.context.RequestContext;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -80,6 +81,10 @@ public class DefuncionHistoricoBean extends DefuncionesPrincipalBean implements 
             externalContext.redirect(externalContext.getRequestContextPath()
                     .concat(ConstantesComunes.DETALLE_DEFUNCION));
         } else {
+
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
+                    FacesMessage.SEVERITY_ERROR,"Ocurri\u00f3 un error al guardar el registro.", ""));
+            RequestContext.getCurrentInstance().execute("errorDialog.show()");
 
         }
     }
