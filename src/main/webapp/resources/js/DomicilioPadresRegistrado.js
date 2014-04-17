e477856171b7f48153b5f6f6c3477ecf0fa80878
Alejandro Diaz Solis <adsolis@sintelti.com.mx>
2014-04-17 13:50:22 -0500

    se completa funcionalidad de asignar domicilio de madre a registrado

---
+++ src/main/webapp/resources/js/DomicilioPadresRegistrado.js
@@ -0,0 +1,30 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ale
+ * Date: 17/04/14
+ * Time: 12:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+
+function asignaCalleMadreRegistrado(elemento) {
+    document.getElementById('formRegistroNormal:tabView:calleRegistrado').value = elemento.value;
+}
+
+
+function asignaColoniaMadreRegistrado(elemento) {
+    document.getElementById('formRegistroNormal:tabView:coloniaRegistrado').value = elemento.value;
+}
+
+function asignaNumeroExteriorMadreRegistradoo(elemento) {
+    document.getElementById('formRegistroNormal:tabView:noExteriorRegistrado').value = elemento.value;
+}
+
+function asignaNumeroInteriorMadreRegistrado(elemento) {
+    document.getElementById('formRegistroNormal:tabView:noInteriorRegistrado').value = elemento.value;
+}
+
+function asignaCodigoPostalMadreRegistrado(elemento) {
+    document.getElementById('formRegistroNormal:tabView:codigoPostalRegistrado').value = elemento.value;
+}
