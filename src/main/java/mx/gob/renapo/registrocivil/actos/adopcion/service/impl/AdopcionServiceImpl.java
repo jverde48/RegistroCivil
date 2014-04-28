9bc06d23f09a6b64e3bf9115cc941dc3c80fd645
migueler <angelespinosarivera@gmail.com>
2014-04-28 05:35:06 -0500

    Correcciones

--- src/main/java/mx/gob/renapo/registrocivil/actos/adopcion/service/impl/AdopcionServiceImpl.java
+++ src/main/java/mx/gob/renapo/registrocivil/actos/adopcion/service/impl/AdopcionServiceImpl.java
@@ -7,12 +7,14 @@ import mx.gob.renapo.registrocivil.actos.adopcion.entity.Adopcion;
 import mx.gob.renapo.registrocivil.actos.adopcion.service.AdopcionService;
 import mx.gob.renapo.registrocivil.catalogos.dto.LocalidadDTO;
 import mx.gob.renapo.registrocivil.catalogos.dto.OficialiaDTO;
+import mx.gob.renapo.registrocivil.catalogos.entity.CatInegiLocalidad;
 import mx.gob.renapo.registrocivil.util.UtileriaService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -55,8 +57,19 @@ public class AdopcionServiceImpl implements AdopcionService{
     }
 
     @Override
-    public List<AdopcionDTO> consultaAdopcionPorNumeroActa(int i, String s) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public List<AdopcionDTO> consultaAdopcionPorNumeroActa(int anio, String cadena) {
+        List<AdopcionDTO> adopcionDTOs =  new ArrayList<AdopcionDTO>();
+        List<Adopcion> adopciones =  new ArrayList<Adopcion>();
+
+        try {
+            adopciones = adopcionDAO.consultaActaNumeroActaAnioRegistro(anio,cadena);
+            if (adopciones!=null && !adopciones.isEmpty())
+                for (Adopcion adopcion:adopciones)
+                    adopcionDTOs.add(mapearEntityADtoAdopcion(adopcion));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return adopcionDTOs;
     }
 
     @Override
@@ -78,9 +91,28 @@ public class AdopcionServiceImpl implements AdopcionService{
     }
 
     private AdopcionDTO mapearEntityADtoAdopcion(Adopcion adopcionEntity) {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(adopcionEntity.getFechaRegistro());
         adopcionDTO.setPersona(utileria.mapearEntityDTOPersona(adopcionEntity.getAdoptado()));
         adopcionDTO.setAdoptante(utileria.mapearEntityDTOPersona(adopcionEntity.getAdoptanteDos()));
         adopcionDTO.setProgenitor(utileria.mapearEntityDTOPersona(adopcionEntity.getAdoptanteUno()));
+        //adopcionDTO.getActaAdopcion().setLocalidadRegistro(adopcionEntity.getA);
+        //adopcionDTO.getActaAdopcion().setOficialia(adopcionEntity.getOficialia());
+        adopcionDTO.getActaAdopcion().setCadena(adopcionEntity.getCadena());
+        //adopcionDTO.getActaAdopcion().setEntidadRegistro(adopcionEntity.getLocalidadRegistro().getMunicipio().);
+        //adopcionDTO.getActaAdopcion().setMunicipioRegistro(adopcionEntity.getLocalidadRegistro().getMunicipio());
+        adopcionDTO.getActaAdopcion().setFoja(adopcionEntity.getFoja());
+        adopcionDTO.getActaAdopcion().setLibro(adopcionEntity.getLibro());
+        adopcionDTO.getActaAdopcion().setAnioRegistro(String.valueOf(calendar.get(Calendar.YEAR)));
+        adopcionDTO.getActaAdopcion().setFechaRegistro(adopcionEntity.getFechaRegistro());
+
+        if (adopcionEntity.getPadreUnoAdoptante() != null)
+            adopcionDTO.setPadreUnoAdoptante(utileria.mapearEntityDTOPersona(adopcionEntity.getPadreUnoAdoptante()));
+        if (adopcionEntity.getPadreDosAdoptante() != null)
+            adopcionDTO.setPadreDosAdoptante(utileria.mapearEntityDTOPersona(adopcionEntity.getPadreDosAdoptante()));
+        adopcionDTO.setTestigoUno(utileria.mapearEntityDTOPersona(adopcionEntity.getTestigoUno()));
+        adopcionDTO.setTestigoDos(utileria.mapearEntityDTOPersona(adopcionEntity.getTestigoDos()));
 
         return adopcionDTO;
     }
@@ -108,6 +140,13 @@ public class AdopcionServiceImpl implements AdopcionService{
         adopcionEntity.setAdoptado(utileria.mapearDtoAEntityPersona(adopcionDTO.getPersona()));
         adopcionEntity.setAdoptanteUno(utileria.mapearDtoAEntityPersona(adopcionDTO.getProgenitor()));
         adopcionEntity.setAdoptanteDos(utileria.mapearDtoAEntityPersona(adopcionDTO.getAdoptante()));
+
+        if (padreUnoAdoptante)
+            adopcionEntity.setPadreUnoAdoptante(utileria.mapearDtoAEntityPersona(adopcionDTO.getPadreUnoAdoptante()));
+        if (padreDosAdoptante)
+            adopcionEntity.setPadreDosAdoptante(utileria.mapearDtoAEntityPersona(adopcionDTO.getPadreDosAdoptante()));
+        adopcionEntity.setTestigoUno(utileria.mapearDtoAEntityPersona(adopcionDTO.getTestigoUno()));
+        adopcionEntity.setTestigoDos(utileria.mapearDtoAEntityPersona(adopcionDTO.getTestigoDos()));
 
         return  adopcionEntity;
     }
