0e40df3218c8e62cb9feefd3ece176436a369f7a
Alejandro Diaz <adsolis@sintelti.com.mx>
2014-04-28 05:38:13 -0500

    se agrega afectacion de personas(contrayentes) tras alta de acta de matrimonio

--- src/main/java/mx/gob/renapo/registrocivil/actos/matrimonio/dao/impl/MatrimonioDAOImpl.java
+++ src/main/java/mx/gob/renapo/registrocivil/actos/matrimonio/dao/impl/MatrimonioDAOImpl.java
@@ -2,8 +2,15 @@ package mx.gob.renapo.registrocivil.actos.matrimonio.dao.impl;
 
 import mx.gob.renapo.registrocivil.actos.matrimonio.dao.MatrimonioDAO;
 import mx.gob.renapo.registrocivil.actos.matrimonio.entity.Matrimonio;
+import mx.gob.renapo.registrocivil.actos.nacimiento.entity.Nacimiento;
 import mx.gob.renapo.registrocivil.comun.dao.impl.GeneralDAOImpl;
+import mx.gob.renapo.registrocivil.comun.dao.impl.TriggerDAOImpl;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,5 +22,39 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class MatrimonioDAOImpl extends GeneralDAOImpl<Matrimonio> implements MatrimonioDAO {
+
+    private static Logger logger = Logger.getLogger(MatrimonioDAOImpl.class);
+
+    @Autowired
+    private TriggerDAOImpl triggerDAO;
+
+
+    @Override
+    /**
+     * Metodo para guardar un registro en la BBDD
+     * @param entidad parametro con la entidad a persistir
+     */
+    @Transactional
+    public Matrimonio guardarRegistro(Matrimonio matrimonio) throws Exception {
+        Session session = getSession();
+        try {
+            session.beginTransaction();
+            session.save(matrimonio);
+            session.getTransaction().commit();
+            triggerDAO.actualizaEstadoCivilContrayentesMatrimonio(matrimonio);
+
+        } catch (Exception e) {
+            logger.info(e.getMessage());
+            e.printStackTrace();
+            session.getTransaction().rollback();
+            throw e;
+        }
+        finally {
+//            session.close();
+        }
+        return matrimonio;
+
+    }
+
 
 }
